78f7a795efa5c877166ff865ccc77b068991052f
Zacharyjg <guldb016@umn.edu>
2021-06-25 03:57:28 -0500

    Added update functions for weather and forecast as it would just append new city on top of previous city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
 let today = moment();
-let city = "Lakeville";
+let city = "";
 
 const iconSrc = "http://openweathermap.org/img/wn/"
 const units = "imperial";
 
 const apiKey = "058dc32d2aa0d74282afc490c90bb317";
 
-let weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=" + units;
-
 const forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=";
-let currentWeatherCard = $("#currentWeather");
+let currentWeatherCard = $("#current-weather");
 let forecastRow = $("#forecast");
 
 function currentWeatherQueryAndPopulation(url) {
@@ -28,17 +26,22 @@ function currentWeatherQueryAndPopulation(url) {
 
 
             cityAndDate.attr("class", "card-title");
-            cityAndDate.text(city + " (" + today.format("L") + ")");
+            cityAndDate.attr("id", "city-and-date");
+            cityAndDate.text(data.name + " (" + today.format("L") + ")");
 
             weatherIcon.attr("src", iconSrc + data.weather[0].icon + "@2x.png");
+            weatherIcon.attr("id", "weather-icon");
 
             temperature.attr("class", "card-text");
+            temperature.attr("id", "weather-temp");
             temperature.text("Temp: " + data.main.temp + "°F");
 
             windSpeed.attr("class", "card-text");
+            windSpeed.attr("id", "weather-wind-speed");
             windSpeed.text("Wind: " + data.wind.speed + " MPH");
 
             humidity.attr("class", "card-text");
+            humidity.attr("id", "weather-humidity");
             humidity.text("Humidity: " + data.main.humidity + "%");
 
 
@@ -67,9 +70,10 @@ function forecastQueryAndPopulation(lon, lat, url) {
         .then(function (data) {
             let uvi = $("<h4>");
             uvi.attr("class", "card-text");
+            uvi.attr("id", "weather-uvi");
             uvi.text("UV Index: " + data.current.uvi);
             currentWeatherCard.append(uvi);
-            
+
             console.log(data);
             for (let i = 0; i < 5; i++) {
                 let day = data.daily[i];
@@ -81,25 +85,32 @@ function forecastQueryAndPopulation(lon, lat, url) {
                 let temperature = $("<h5>");
                 let windSpeed = $("<h5>");
                 let humidity = $("<h5>");
-    
+
                 forecastCard.attr("class", "col-2 card");
+                forecastCard.attr("id", "forecast-card-" + i);
 
                 date.attr("class", "card-title");
+                date.attr("id", "forecast-date-" + i);
                 date.text("(" + today.add(1, "days").format("L") + ")");
-    
+
                 weatherIconHeader.attr("class", "forecast-image-heading");
+
                 weatherIcon.attr("src", iconSrc + day.weather[0].icon + "@2x.png");
                 weatherIcon.attr("class", "forecast-image");
+                weatherIcon.attr("id", "forecast-image-" + i);
 
                 temperature.attr("class", "card-text");
+                temperature.attr("id", "forecast-temp-" + i);
                 temperature.text("Temp: " + day.temp.day + "°F");
-    
+
                 windSpeed.attr("class", "card-text");
+                windSpeed.attr("id", "forecast-wind-" + i);
                 windSpeed.text("Wind: " + day.wind_speed + " MPH");
-    
+
                 humidity.attr("class", "card-text");
+                humidity.attr("id", "forecast-humidity-" + i);
                 humidity.text("Humidity: " + day.humidity + "%");
-    
+
                 weatherIconHeader.append(weatherIcon);
                 forecastCard.append(date);
                 forecastCard.append(weatherIconHeader);
@@ -111,12 +122,86 @@ function forecastQueryAndPopulation(lon, lat, url) {
         });
 }
 
-
-
 function citySearch(cityName) {
-    city = cityName;
-    let cityAndDate = $("<h1>");
-
+    let weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=" + units;
+    if (city == "") {
+        city = cityName;
+        currentWeatherQueryAndPopulation(weatherUrl);
+    } else {
+        city = cityName;
+        today.subtract(5, "days");
+        updateWeather(weatherUrl);
+    }
 }
-console.log(today.format("L"));
-currentWeatherQueryAndPopulation(weatherUrl);
+
+function updateWeather(url) {
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            let cityAndDate = $("#city-and-date");
+            let weatherIcon = $("#weather-icon");
+            let temperature = $("#weather-temp");
+            let windSpeed = $("#weather-wind-speed");
+            let humidity = $("#weather-humidity");
+
+            cityAndDate.text(data.name + " (" + today.format("L") + ")");
+
+            weatherIcon.attr("src", iconSrc + data.weather[0].icon + "@2x.png");
+
+            temperature.text("Temp: " + data.main.temp + "°F");
+
+            windSpeed.text("Wind: " + data.wind.speed + " MPH");
+
+            humidity.text("Humidity: " + data.main.humidity + "%");
+
+            cityAndDate.append(weatherIcon);
+            
+            let lon = data.coord.lon;
+            let lat = data.coord.lat;
+            updateForecast(lon, lat, forecastUrl);
+        });
+}
+
+function updateForecast(lon, lat, url) {
+    url = url + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=" + units + "&appid=" + apiKey;
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let uvi = $("#weather-uvi");
+            uvi.text("UV Index: " + data.current.uvi);
+
+            console.log(data);
+            for (let i = 0; i < 5; i++) {
+                let day = data.daily[i];
+
+                let date = $("#forecast-date-" + i);
+                let weatherIcon = $("#forecast-image-" + i);
+                let temperature = $("#forecast-temp-" + i);
+                let windSpeed = $("#forecast-wind-" + i);
+                let humidity = $("#forecast-humidity-" + i);
+
+                date.text("(" + today.add(1, "days").format("L") + ")");
+
+                weatherIcon.attr("src", iconSrc + day.weather[0].icon + "@2x.png");
+
+                temperature.text("Temp: " + day.temp.day + "°F");
+
+                windSpeed.text("Wind: " + day.wind_speed + " MPH");
+
+                humidity.text("Humidity: " + day.humidity + "%");
+            }
+        });
+}
+
+$("#submitButton").on("click", function (event) {
+    event.preventDefault();
+    console.log($("#inputted-city").val());
+    citySearch($("#inputted-city").val());
+    $("#inputted-city").val("");
+})
